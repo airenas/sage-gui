938062a6e451f5f30d1dfa19fb074bfb685357c9
Airenas Vaičiūnas <airenass@gmail.com>
2022-08-31 17:07:36 +0300

    Fix docker build

--- build/docker/entrypoint.sh
+++ build/docker/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo Updating base href to ${BASE_HREF}
-cat /usr/share/nginx/html/index.html | envsubst '${BASE_HREF}' > /usr/share/nginx/html/index.html
+cat /usr/share/index.html.in | envsubst '${BASE_HREF}' > /usr/share/nginx/html/index.html
 
